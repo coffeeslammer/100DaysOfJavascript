362362580b780baf3636198f3f3695122d337a8a
Don Christensen <noobgrammer@gmail.com>
2023-10-09 21:06:48 -0700

    CountDownTimer

---
+++ 08CountDownDateTimer/script.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const days = document.querySelector(".days");
+const hours = document.querySelector(".hours");
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const enterDate = document.querySelector("input");
+const btn = document.querySelector(".btn");
+
+let dateEntered = "";
+
+function countingDown() {
+  const futureDate = new Date(dateEntered);
+  //all this code I am pretty sure I did it the hard way
+  const presentDate = new Date();
+  const daysLeft = Math.floor((futureDate - presentDate) / 1000 / 60 / 60 / 24);
+  const daysWithRemainder = (futureDate - presentDate) / 1000 / 60 / 60 / 24;
+  const hoursRemainder = daysWithRemainder - daysLeft;
+  const hoursLeft = Math.floor(hoursRemainder * 24);
+  const hoursWithRemainder = hoursRemainder * 24;
+  const minutedRemainder = hoursWithRemainder - hoursLeft;
+  const minutesLeft = Math.floor(minutedRemainder * 60);
+  const minutesWithRemainder = minutedRemainder * 60;
+  const secondsRemainder = minutesWithRemainder - minutesLeft;
+  const secondsLeft = Math.floor(secondsRemainder * 60);
+
+  days.innerHTML = daysLeft;
+  hours.innerText = hoursLeft;
+  minutes.textContent = minutesLeft;
+  seconds.textContent = secondsLeft;
+}
+function starting() {
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const temp = new Date(enterDate.value);
+
+  if (temp == "Invalid Date") {
+    document.querySelector(".error").textContent = "Invalid Input";
+  } else {
+    document.querySelector(".error").textContent = "";
+    document.querySelector(".countdown-container").classList.remove("hidden");
+    document.querySelector(".enter-date").classList.add("hidden");
+    dateEntered = enterDate.value;
+    document.querySelector(".countingTo").textContent = `${
+      monthNames[temp.getMonth()]
+    } ${temp.getDate()}  ${temp.getFullYear()}  `;
+    countingDown();
+    setInterval(countingDown, 1000);
+  }
+}
+
+btn.addEventListener("click", starting);
+// countingDown();
